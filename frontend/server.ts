34095d3f5d25a3b5be2584caaee0e56fcb2459aa
dvizioon <danielmartinsjob@gmail.com>
2026-07-23 18:25:13 -0300

    Refactor static file serving in server.ts to use a variable for the distribution path

--- frontend/server.ts
+++ frontend/server.ts
@@ -3,14 +3,15 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const distPath = path.join(__dirname,"..", "dist");
 const app = express();
 const NODE_ENV = process.env.VITE_NODE_ENV || 'development';
 const PORT = process.env.VITE_PORT || 5173;
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(distPath));
 
 app.use((req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 app.listen(PORT, () => {
